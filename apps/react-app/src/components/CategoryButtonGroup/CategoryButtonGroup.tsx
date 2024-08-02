565d085fb4f7af6ae89955c7797f18a7e0435cd5
Eduardo de Leon <eduardo.de.leon@accenture.com>
2024-08-02 14:33:28 -0600

    Activities 1 and 2

--- apps/react-app/src/components/CategoryButtonGroup/CategoryButtonGroup.tsx
+++ apps/react-app/src/components/CategoryButtonGroup/CategoryButtonGroup.tsx
@@ -1,42 +1,42 @@
-import { ButtonGroup } from "@mui/material";
+import { ButtonGroup } from '@mui/material';
 
-import { Container, StyledButton } from "./CategoryButtonGroup.styles";
+import { Container, StyledButton } from './CategoryButtonGroup.styles';
+import { ReactNode } from 'react';
 
-// const categoryOptions = [
-//   {
-//     key: "all",
-//     name: "All",
-//   },
-//   {
-//     key: "healt",
-//     name: "Health",
-//   },
-//   {
-//     key: "travel",
-//     name: "Travel",
-//   },
-//   {
-//     key: "sports",
-//     name: "Sports",
-//   },
-// ];
+const categoryOptions = [
+  {
+    key: 'all',
+    name: 'All'
+  },
+  {
+    key: 'healt',
+    name: 'Health'
+  },
+  {
+    key: 'travel',
+    name: 'Travel'
+  },
+  {
+    key: 'sports',
+    name: 'Sports'
+  }
+];
+
+const buttons: ReactNode[] = [];
+
+categoryOptions.forEach((category) => {
+  buttons.push(
+    <StyledButton type="button" selected={false}>
+      {category.name}
+    </StyledButton>
+  );
+});
 
 function CategoryButtonGroup() {
   return (
     <Container item>
       <ButtonGroup aria-label="category button group" color="inherit">
-        <StyledButton type="button" selected={false}>
-          {/* Activity 1 - Render category name */}
-        </StyledButton>
-        <StyledButton type="button" selected={false}>
-          {/* Activity 1 - Render category name */}
-        </StyledButton>
-        <StyledButton type="button" selected={false}>
-          {/* Activity 1 - Render category name */}
-        </StyledButton>
-        <StyledButton type="button" selected={false}>
-          {/* Activity 1 - Render category name */}
-        </StyledButton>
+        {buttons}
       </ButtonGroup>
     </Container>
   );
