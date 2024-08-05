48843d48af558f342cdf174c98552f05329cbb04
Eduardo de Leon <eduardo.de.leon@accenture.com>
2024-08-05 16:23:37 -0600

    Activity 6

--- apps/react-app/src/components/Page/CategoriesPage/CategoriesPage.tsx
+++ apps/react-app/src/components/Page/CategoriesPage/CategoriesPage.tsx
@@ -1,28 +1,57 @@
-import { Grid } from '@mui/material';
+import { useState } from 'react';
+import { Grid, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 import { PageContainer } from './CategoriesPage.styles';
-// import { Category } from "../../../types";
+import { Category } from '../../../types';
 
-// const categories: Category[] = [
-//   { id: "663fef70d513515319551d1f", name: "Travel" },
-//   { id: "663fef70d513515319546d1f", name: "Food" },
-// ];
+const categories: Category[] = [
+  { id: '663fef70d513515319551d1f', name: 'Travel' },
+  { id: '663fef70d513515319546d1f', name: 'Food' }
+];
 
 function CategoriesPage() {
-  // ACT 6 - Create a state called "rows"
-  // ACT 6 - Call setRows when the component is mounted for first time, use "categories" variable as new value.
+  const [rows] = useState(categories);
 
-  //ACT 6 - Create two empty functions called "handleEditItem" and "handleDeleteItem"
+  function handleEditItem() {}
+  function handleDeleteItem() {}
   return (
     <PageContainer container>
       Categories Page
       <Grid item sx={{ justifyContent: 'flex-end', display: 'flex' }}>
-        //Add category (Icon button)
+        {/* Add category (Icon button) */}
       </Grid>
       <Grid item sx={{ flexGrow: 1 }}>
-        {/* ACT 6 - Create a component called "Table" to display category names */}
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell>
+                    <IconButton aria-label="delete" onClick={handleDeleteItem}>
+                      <DeleteIcon />
+                    </IconButton>
+                    <IconButton aria-label="edit" onClick={handleEditItem}>
+                      <EditIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
-      //Modal
+      {/* Modal */}
     </PageContainer>
   );
 }
