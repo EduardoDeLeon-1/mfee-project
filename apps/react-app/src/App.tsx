7eab1abf8275230abad4210cb776fa6f299d9142
Eduardo de Leon <eduardo.de.leon@accenture.com>
2024-08-05 16:21:51 -0600

    Fix errors

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -1,32 +1,24 @@
 import { Grid } from '@mui/material';
 
-import HomePage from './components/HomePage';
-import NavBar from "./components/NavBar";
-import PostPage from './components/PostPage';
+import NavBar from './components/NavBar';
+import { CategoriesPage, HomePage, LoginPage, PostPage } from './components/Page';
 import { PostProvider } from './context';
 
 function App() {
-  const page: string = 'HomePage';
+  const page: string = 'CategoriesPage';
   return (
-    // ACT 7 - Rneder SnackbarProvider component
+    // ACT 7 - Render SnackbarProvider component
     <PostProvider>
-      <>
-        <Grid container id="app" direction="column" height="100vh" wrap="nowrap">
-          <NavBar />
-          <Grid
-            container
-            item
-            wrap="nowrap"
-            sx={{ display: "flex", flexDirection: "column", height: "calc(100vh - 84px)" }}
-          >
-            {page === 'HomePage' && <HomePage />}
-            {page === 'PostPage' && <PostPage />}
-            {page === 'LoginPage' && <LoginPage />}
-            {page === 'CategoriesPage' && <CategoriesPage />}
-          </Grid>
+      <Grid container id="app" direction="column" height="100vh" wrap="nowrap">
+        <NavBar />
+        <Grid container item wrap="nowrap" sx={{ display: 'flex', flexDirection: 'column', height: 'calc(100vh - 84px)' }}>
+          {page === 'HomePage' && <HomePage />}
+          {page === 'PostPage' && <PostPage />}
+          {page === 'LoginPage' && <LoginPage />}
+          {page === 'CategoriesPage' && <CategoriesPage />}
         </Grid>
-      </>
-    <PostProvider />
+      </Grid>
+    </PostProvider>
   );
 }
 
